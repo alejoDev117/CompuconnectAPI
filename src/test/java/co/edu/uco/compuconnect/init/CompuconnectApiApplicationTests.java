7c55576375f66770c2f15cfd7b51a79a3627de70
Alejandro Gómez Orjuela <102133299+alejoDev117@users.noreply.github.com>
2023-05-11 16:09:28 -0500

    Initial commit

---
+++ src/test/java/co/edu/uco/compuconnect/init/CompuconnectApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uco.compuconnect.init;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompuconnectApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
