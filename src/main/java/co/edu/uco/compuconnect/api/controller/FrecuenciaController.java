a644785d86462cf8c9bb43f31e10077599a74e04
SkDanielP <113185316+SkDanielP@users.noreply.github.com>
2023-05-15 12:47:24 -0500

    Update FrecuenciaController.java

--- src/main/java/co/edu/uco/compuconnect/api/controller/FrecuenciaController.java
+++ src/main/java/co/edu/uco/compuconnect/api/controller/FrecuenciaController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import co.edu.uco.compuconnect.api.controller.response.Response;
 import co.edu.uco.compuconnect.business.facade.FrecuenciaFacade;
